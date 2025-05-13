9513af442ab9ae92f86f3f282c04d39738677c6a
Iskander2323 <iska200314@gmail.com>
2025-05-13 18:42:15 +0500

    created grid view page

---
+++ lib/components/pages/grid_view_page/grid_view_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GridViewPage extends StatelessWidget {
+  const GridViewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Grid View Page')),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1.0,
+        ),
+        itemBuilder: (context, index) {
+          return Card(child: Center(child: Text('Item $index')));
+        },
+      ),
+    );
+  }
+}
