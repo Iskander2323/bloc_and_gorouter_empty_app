73006154a296bebbd41c26e4adc5307135deb788
Iskander2323 <iska200314@gmail.com>
2025-05-13 17:03:20 +0500

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,6 @@
-import 'package:bloc_and_go_router_empty_app/components/ui/app_page.dart';
+import 'package:bloc_and_go_router_empty_app/components/app_page/ui/app_page.dart';
+import 'package:bloc_and_go_router_empty_app/components/pages/list_view_page/ui/list_view_page.dart';
+import 'package:bloc_and_go_router_empty_app/components/pages/widgets_page/widgets_page.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter routes = GoRouter(
@@ -8,6 +10,18 @@ final GoRouter routes = GoRouter(
       path: '/main',
       name: 'main',
       builder: (context, state) => AppPage(),
+      routes: [
+        GoRoute(
+          path: 'widgets_page',
+          name: 'widgets_page',
+          builder: (context, state) => const WidgetsPage(),
+        ),
+        GoRoute(
+          path: 'list_view_page',
+          name: 'list_view_page',
+          builder: (context, state) => const ListViewPage(),
+        ),
+      ],
     ),
   ],
 );
